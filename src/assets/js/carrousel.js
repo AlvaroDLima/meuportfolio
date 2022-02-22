e8b6a17437bac1b26fa89000d97560c3bd8b5473
ramonlopz1 <davidramon123@hotmail.com>
2022-02-22 13:46:02 -0300

    carrousel

--- src/assets/js/carrousel.js
+++ src/assets/js/carrousel.js
@@ -1,23 +1,26 @@
-const cards = document.querySelectorAll('#section-skills .cards')
-const aside = document.querySelectorAll('#setion-skills aside')
-let count = 500
+let aside = document.querySelector('#section-skills aside')
+let allCards = document.querySelector('#section-skills .all-cards')
+let cards = document.querySelectorAll('#section-skills .cards')
+
 let array = Array.from(cards)
+let newAside = allCards.cloneNode(true)
+
+let translateValue = 500
 
 function carrousel() {
-    array.forEach(card => {
-        count = count - 0.1
-        card.style.transform = `translateX(${count}px)`
+    array.forEach(() => {
+        translateValue = translateValue - 0.1
+        allCards.style.transform = `translateX(${translateValue}px)`
+        newAside.style.transform = `translateX(${translateValue}px)`
 
-            if(count < -330) {
-                aside.insertAdjacentHTML('beforeend', array[0])
-            }
+        if(translateValue < -47 && translateValue > -50) {
+           
+            aside.appendChild(newAside)
+        }
+        
     })
 }
 
-
 setInterval(() => {
     carrousel()
-}, 5);        
-
-
-
+}, 5);
