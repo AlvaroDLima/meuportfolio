9b39fc94c74b648126eafbeb95cb33e8ad86d62e
ramonlopz1 <davidramon123@hotmail.com>
2022-02-24 13:30:46 -0300

    create

--- src/assets/js/jobs.js
+++ src/assets/js/jobs.js
@@ -1,39 +1,55 @@
+// DOM ELEMENTS
 const cards = document.querySelectorAll('#section__experienciaprofissional-circles .circle')
-
 const btns = document.querySelectorAll('#section__experienciaprofissional-circles .circle button')
 
-let currentCard = ""
-
+// CREATE NEW ELEMENTS
 const newDiv = document.createElement('div')
+    newDiv.classList.add('newDivJS')
+
 const newTitle = document.createElement('h3')
 const newParagraph = document.createElement('p')
 
-newDiv.classList.add('newDivJS')
+// AUXILIARY VARIABLES
+let currentCard = ""
+let clicked = false
 
 
+// CHANGE CSS PARAMETERS
+function animateCSS(btn, index, display, rotate, width, radius, justContent, addDiv) {
+    btn.style.transform = `rotate(${rotate}deg)`
+    
+    currentCard = cards[index]
+    currentCard.style.width = `${width}px`
+    currentCard.style.borderRadius = `${radius}px`
+    currentCard.style.justifyContent = justContent
+    
 
+    addDiv == true ? currentCard.appendChild(newDiv) : newDiv.remove()
+        
+
+    if(currentCard.hasAttribute('fst')) {
+        newTitle.innerHTML = '2018 - 2020'
+        newParagraph.innerHTML = 'Controle/elaboração de Dashboards no software EXCEL, relatórios de estatísticas de vendas, faturamento e despesas mensais, gerenciamento no software DataWeb.'
+        newDiv.appendChild(newTitle)
+        newDiv.appendChild(newParagraph)
+    }
+
+    cards.forEach(card => {
+        if(card != currentCard) 
+            card.style.display = display
+    })
+}
+
+
+// BTNS CLICK EVENT TO EXECUTE FUNCTIONS
 btns.forEach((btn, index) => {
     btn.onclick = () => {
-        btn.style.transform = `rotate(180deg)`
-        
-        currentCard = cards[index]
-        currentCard.style.width = `${600}px`
-        currentCard.style.borderRadius = `${6}px`
-        currentCard.style.justifyContent = 'space-evenly'
-        currentCard.appendChild(newDiv)
-        
-
-        if(currentCard.hasAttribute('fst')) {
-            newTitle.innerHTML = '2018 - 2020'
-            newParagraph.innerHTML = 'Controle/elaboração de Dashboards no software EXCEL, relatórios de estatísticas de vendas, faturamento e despesas mensais, gerenciamento no software DataWeb.'
-            newDiv.appendChild(newTitle)
-            newDiv.appendChild(newParagraph)
+        if(clicked == false) {
+            animateCSS(btn, index, 'none', 180, 600, 6, 'space-evenly', true)
+            clicked = true
+        } else if (clicked == true) {
+            animateCSS(btn, index, 'flex', 360, 100, 100, 'center', false)
+            clicked = false
         }
-
-        cards.forEach(card => {
-            if(card != currentCard) {
-                card.style.display = 'none'
-            }
-        })
     }
 })
