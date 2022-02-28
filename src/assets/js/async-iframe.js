6a50eda9d9a948134245e51a069beb39ffc41ebe
ramonlopz1 <davidramon123@hotmail.com>
2022-02-28 13:29:01 -0300

    responsive

--- src/assets/js/async-iframe.js
+++ src/assets/js/async-iframe.js
@@ -2,5 +2,4 @@ const iframes = document.querySelectorAll('iframe');
 const array = Array.from(iframes)
 console.log(iframes)
 iframes.forEach((iframe, index) => {
-    console.log(iframe)
 })
--- src/assets/js/header.js
+++ src/assets/js/header.js
@@ -1,5 +1,7 @@
+let header = document.querySelector('header');
+
+//ANIMATION
 const changeHeaderClass = () => {
-    let header = document.querySelector('header');
     
     window.addEventListener('scroll', () => {
         if (window.scrollY > 1) {
@@ -9,3 +11,40 @@ const changeHeaderClass = () => {
         }
     });
 }; changeHeaderClass();
+
+
+
+
+
+// MENU MOBILE
+const menuMobile = () => {
+    const displayWidth = document.querySelector('body').clientWidth
+    let nav = document.querySelector('header nav')
+    let btnHambur = document.createElement('button')
+    let ul = document.querySelector('nav ul')
+
+    btnHambur.innerHTML = '='
+    
+    if(displayWidth <= 1150) {
+        nav.insertAdjacentElement('afterbegin', btnHambur)
+    } else {
+        nav.style.display = 'flex';
+        ul.style.display = 'flex'
+    }
+
+    let clicked = true
+    btnHambur.addEventListener('click', () => {
+        if(clicked) {
+            header.style.height = `${430}px`;
+            nav.style.display = 'flex';
+            ul.style.display = 'flex'
+            clicked = false
+        } else if (!clicked) {
+            header.style.height = `${80}px`;
+            ul.style.display = 'none'
+            clicked = true
+        }
+    })
+}
+
+menuMobile()
