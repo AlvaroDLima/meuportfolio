e25e52a0bf186e7e0073896bd63aa32a0e4d88f2
ramonlopz1 <davidramon123@hotmail.com>
2022-03-04 21:37:32 -0300

    acessibilidade web

--- src/assets/js/carrouselAnimation.js
+++ src/assets/js/carrouselAnimation.js
@@ -1,30 +1,23 @@
-let firstDivCards = document.querySelector('#section-skills .all-cards:nth-of-type(1)');
-let secondDivCards = firstDivCards.cloneNode(true);
+let allCards = document.querySelector('#section-skills .all-cards');
+
+let btns = document.querySelectorAll('.section-skills-btn')
 let aside = document.querySelector('#section-skills aside')
-    aside.appendChild(secondDivCards);
+    
+let translateValue = 0;
 
-let containerSize = firstDivCards.offsetWidth;
-let translateValue = 1;
-
-function carrousel() {
-        translateValue--;
-
-        if(translateValue > -1538) {
-            firstDivCards.style.transform = `translateX(${translateValue}px)`
-            secondDivCards.style.transform = `translateX(${translateValue}px)`
-        } else if (translateValue <= -1538) {
-            firstDivCards.style.transform = `translateX(${(containerSize*2)}px)`
-            secondDivCards.style.transform = `translateX(${(containerSize*2)}px)`
-            translateValue = 1
-        }
+function carrousel(index) {
+    if (index == 0 && translateValue <= -130) {
+        translateValue += 130
+        allCards.style.transform = `translateX(${translateValue}px)`
+    } else if (index == 1 && translateValue > -910) {
+        translateValue -= 130
+        allCards.style.transform = `translateX(${translateValue}px)`
+    }
 }
-
-let refreshInterval = ""
-
-aside.addEventListener('mouseenter', () => {
-    refreshInterval = setInterval(carrousel, 7)
-})
-
-aside.addEventListener('mouseleave', () => {
-    clearInterval(refreshInterval)
+    
+btns.forEach((elem, index) => {
+    
+    elem.addEventListener('click', () => {
+            carrousel(index)
+    })
 })
