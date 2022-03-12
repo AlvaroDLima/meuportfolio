fabedfbac0b17422e8f8ca56c86faed732379feb
ramonlopz1 <rmnprojetos@outlook.com>
2022-03-11 23:01:30 -0300

    adjustments in carousel buttons

--- src/assets/js/carrouselAnimation.js
+++ src/assets/js/carrouselAnimation.js
@@ -4,11 +4,11 @@ let btns = document.querySelectorAll('.section-skills-btn')
 let aside = document.querySelector('#section-skills aside')
     
 let translateValue = 0;
-
+let screenResolution = ""
 function carrousel(index) {
-    let screenResolution = window.innerWidth;
-    
-    if (screenResolution => 1900) {
+    screenResolution = window.innerWidth
+   
+    if (screenResolution >= 1400) {
         
         if (index == 0 && translateValue <= -170) {
             translateValue += 170
@@ -17,8 +17,30 @@ function carrousel(index) {
             translateValue -= 170
             allCards.style.transform = `translateX(${translateValue}px)`
         }
-    } else if (screenResolution < 1900) {
-        console.log('oi')
+    } else if (screenResolution > 900 && screenResolution < 1400) {
+        if (index == 0 && translateValue <= -170) {
+            translateValue += 170
+            allCards.style.transform = `translateX(${translateValue}px)`
+        } else if (index == 1 && translateValue > -1080) {
+            translateValue -= 170
+            allCards.style.transform = `translateX(${translateValue}px)`
+        }
+    } else if (screenResolution > 750 && screenResolution < 900) {
+        if (index == 0 && translateValue <= -170) {
+            translateValue += 170
+            allCards.style.transform = `translateX(${translateValue}px)`
+        } else if (index == 1 && translateValue > -1250) {
+            translateValue -= 170
+            allCards.style.transform = `translateX(${translateValue}px)`
+        }
+    } else if (screenResolution < 750) {
+        if (index == 0 && translateValue <= -170) {
+            translateValue += 170
+            allCards.style.transform = `translateX(${translateValue}px)`
+        } else if (index == 1 && translateValue > -1980) {
+            translateValue -= 170
+            allCards.style.transform = `translateX(${translateValue}px)`
+        }
     }
 }
     
